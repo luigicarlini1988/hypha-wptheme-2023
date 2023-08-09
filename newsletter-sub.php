027f4377a4b56d7e0d08af8aac39eb2ca204fae0
Luigi Carlini <luigicarlini88@gmail.com>
2023-08-09 15:46:55 +0800

    form work 1/3

--- newsletter-sub.php
+++ newsletter-sub.php
@@ -47,25 +47,29 @@
                                 
                             <div class="mauticform-page-wrapper mauticform-page-1" data-mautic-form-page="1">
 
-                                <div id="mauticform_newslettersignup2023_email" data-validate="email" data-validation-type="email" class="mauticform-row mauticform-email mauticform-field-1 mauticform-required">
+                                <div id="mauticform_newslettersignup2023_email" data-validate="email" data-validation-type="email" class="mauticform-row mauticform-email mauticform-field-1 mauticform-required wrap-area full">
                                     <label id="mauticform_label_newslettersignup2023_email" for="mauticform_input_newslettersignup2023_email" class="mauticform-label">Email</label>
                                     <input id="mauticform_input_newslettersignup2023_email" name="mauticform[email]" value="" class="mauticform-input" type="email">
                                     <span class="mauticform-errormsg" style="display: none;">This is required.</span>
                                 </div>
 
-                                <div id="mauticform_newslettersignup2023_first_name" data-validate="first_name" data-validation-type="text" class="mauticform-row mauticform-text mauticform-field-2 mauticform-required">
+                                <div id="mauticform_newslettersignup2023_first_name" data-validate="first_name" data-validation-type="text" class="mauticform-row mauticform-text mauticform-field-2 mauticform-required wrap-area half">
                                     <label id="mauticform_label_newslettersignup2023_first_name" for="mauticform_input_newslettersignup2023_first_name" class="mauticform-label">First name</label>
                                     <input id="mauticform_input_newslettersignup2023_first_name" name="mauticform[first_name]" value="" class="mauticform-input" type="text">
                                     <span class="mauticform-errormsg" style="display: none;">This is required.</span>
                                 </div>
 
-                                <div id="mauticform_newslettersignup2023_last_name" data-validate="last_name" data-validation-type="text" class="mauticform-row mauticform-text mauticform-field-3 mauticform-required">
+                                <div id="mauticform_newslettersignup2023_last_name" data-validate="last_name" data-validation-type="text" class="mauticform-row mauticform-text mauticform-field-3 mauticform-required wrap-area half">
                                     <label id="mauticform_label_newslettersignup2023_last_name" for="mauticform_input_newslettersignup2023_last_name" class="mauticform-label">Last Name</label>
                                     <input id="mauticform_input_newslettersignup2023_last_name" name="mauticform[last_name]" value="" class="mauticform-input" type="text">
                                     <span class="mauticform-errormsg" style="display: none;">This is required.</span>
                                 </div>
 
-                                <div id="mauticform_newslettersignup2023_submit" class="mauticform-row mauticform-button-wrapper mauticform-field-4">
+                                <div class="wrap-area half">
+                                    <span class="text-12">By submitting this form you agree to our <a href="<?php echo home_url(); ?>/privacy-policy/" target="_blank" class="light-blu">Privacy Policy</a></span>
+                                </div>
+
+                                <div id="mauticform_newslettersignup2023_submit" class="mauticform-row mauticform-button-wrapper mauticform-field-4 wrap-area half">
                                     <button type="submit" name="mauticform[submit]" id="mauticform_input_newslettersignup2023_submit" value="" class="mauticform-button btn btn-default">Submit</button>
                                 </div>
                                 </div>
@@ -80,7 +84,7 @@
                     </div>
                 </div>
             </div>
-            
+
         </div>
     </section>
 
