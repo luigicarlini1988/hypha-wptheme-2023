38f3b6c1f1df2aa778dce2a87b6217236d62b67c
Luigi Carlini <luigicarlini88@gmail.com>
2023-08-13 09:53:37 +0800

    DAOs

--- incl/daos.php
+++ incl/daos.php
@@ -23,209 +23,189 @@
 
     <div class="area">
         <div id="dao-carousel" class="owl-carousel owl-theme">
-            <a href="">
-                <div class="dao-card">
-                    <div class="wrap flex justify-center align-top">
-                        <div class="top-right-link">
-                            <img src="<?php echo get_template_directory_uri(); ?>/img/icons/top-right-link.svg" />
-                        </div>
-                        <img class="daologos"
-                            src="<?php echo get_template_directory_uri(); ?>/img/logos/logo-round.svg" />
-                        <div style="width:100%">
-                            <p class="text-18">
-                                Short DAO
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </a>
 
-            <a href="">
-                <div class="dao-card">
-                    <div class="wrap flex justify-center align-top">
+            <div class="dao-card">
+                <div class="wrap flex justify-center align-top">
+                    <a href="https://dao.hypha.earth/seedscollaboratory/" target="_blank">
                         <div class="top-right-link">
                             <img src="<?php echo get_template_directory_uri(); ?>/img/icons/top-right-link.svg" />
                         </div>
-                        <img class="daologos"
-                            src="<?php echo get_template_directory_uri(); ?>/img/logos/logo-round.svg" />
-                        <div style="width:100%">
-                            <p class="text-18">
-                                LongNameForHere DAO
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </a>
 
-            <a href="">
-                <div class="dao-card">
-                    <div class="wrap flex justify-center align-top">
-                        <div class="top-right-link">
-                            <img src="<?php echo get_template_directory_uri(); ?>/img/icons/top-right-link.svg" />
-                        </div>
-                        <img class="daologos"
-                            src="<?php echo get_template_directory_uri(); ?>/img/logos/logo-round.svg" />
-                        <div style="width:100%">
-                            <p class="text-18">
-                                Short DAO
-                            </p>
-                        </div>
+                        <img class="daologos" src="<?php echo get_template_directory_uri(); ?>/img/daos/seeds.png" />
+                    </a>
+                    <div style="width:100%">
+                        <p class="text-18">
+                            SEEDS
+                        </p>
                     </div>
-                </div>
-            </a>
 
-            <a href="">
-                <div class="dao-card">
-                    <div class="wrap flex justify-center align-top">
-                        <div class="top-right-link">
-                            <img src="<?php echo get_template_directory_uri(); ?>/img/icons/top-right-link.svg" />
-                        </div>
-                        <img class="daologos"
-                            src="<?php echo get_template_directory_uri(); ?>/img/logos/logo-round.svg" />
-                        <div style="width:100%">
-                            <p class="text-18">
-                                LongNameForHere DAO
-                            </p>
-                        </div>
-                    </div>
                 </div>
-            </a>
+            </div>
 
-            <a href="">
-                <div class="dao-card">
-                    <div class="wrap flex justify-center align-top">
-                        <div class="top-right-link">
-                            <img src="<?php echo get_template_directory_uri(); ?>/img/icons/top-right-link.svg" />
-                        </div>
-                        <img class="daologos"
-                            src="<?php echo get_template_directory_uri(); ?>/img/logos/logo-round.svg" />
-                        <div style="width:100%">
-                            <p class="text-18">
-                                Short DAO
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </a>
 
-            <a href="">
-                <div class="dao-card">
-                    <div class="wrap flex justify-center align-top">
-                        <div class="top-right-link">
-                            <img src="<?php echo get_template_directory_uri(); ?>/img/icons/top-right-link.svg" />
-                        </div>
-                        <img class="daologos"
-                            src="<?php echo get_template_directory_uri(); ?>/img/logos/logo-round.svg" />
-                        <div style="width:100%">
-                            <p class="text-18">
-                                LongNameForHere DAO
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </a>
 
-            <a href="">
-                <div class="dao-card">
-                    <div class="wrap flex justify-center align-top">
+            <div class="dao-card">
+                <div class="wrap flex justify-center align-top">
+                    <a href="https://dao.hypha.earth/localscale/" target="_blank">
                         <div class="top-right-link">
                             <img src="<?php echo get_template_directory_uri(); ?>/img/icons/top-right-link.svg" />
                         </div>
-                        <img class="daologos"
-                            src="<?php echo get_template_directory_uri(); ?>/img/logos/logo-round.svg" />
-                        <div style="width:100%">
-                            <p class="text-18">
-                                Short DAO
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </a>
 
-            <a href="">
-                <div class="dao-card">
-                    <div class="wrap flex justify-center align-top">
-                        <div class="top-right-link">
-                            <img src="<?php echo get_template_directory_uri(); ?>/img/icons/top-right-link.svg" />
-                        </div>
                         <img class="daologos"
-                            src="<?php echo get_template_directory_uri(); ?>/img/logos/logo-round.svg" />
-                        <div style="width:100%">
-                            <p class="text-18">
-                                LongNameForHere DAO
-                            </p>
-                        </div>
+                            src="<?php echo get_template_directory_uri(); ?>/img/daos/localscale.png" />
+                    </a>
+                    <div style="width:100%">
+                        <p class="text-18">
+                            LocalScale
+                        </p>
                     </div>
                 </div>
-            </a>
+            </div>
 
-            <a href="">
-                <div class="dao-card">
-                    <div class="wrap flex justify-center align-top">
-                        <div class="top-right-link">
-                            <img src="<?php echo get_template_directory_uri(); ?>/img/icons/top-right-link.svg" />
-                        </div>
-                        <img class="daologos"
-                            src="<?php echo get_template_directory_uri(); ?>/img/logos/logo-round.svg" />
-                        <div style="width:100%">
-                            <p class="text-18">
-                                Short DAO
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </a>
 
-            <a href="">
-                <div class="dao-card">
-                    <div class="wrap flex justify-center align-top">
-                        <div class="top-right-link">
-                            <img src="<?php echo get_template_directory_uri(); ?>/img/icons/top-right-link.svg" />
-                        </div>
-                        <img class="daologos"
-                            src="<?php echo get_template_directory_uri(); ?>/img/logos/logo-round.svg" />
-                        <div style="width:100%">
-                            <p class="text-18">
-                                LongNameForHere DAO
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </a>
 
-            <a href="">
-                <div class="dao-card">
-                    <div class="wrap flex justify-center align-top">
-                        <div class="top-right-link">
-                            <img src="<?php echo get_template_directory_uri(); ?>/img/icons/top-right-link.svg" />
-                        </div>
-                        <img class="daologos"
-                            src="<?php echo get_template_directory_uri(); ?>/img/logos/logo-round.svg" />
-                        <div style="width:100%">
-                            <p class="text-18">
-                                Short DAO
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </a>
 
-            <a href="">
-                <div class="dao-card">
-                    <div class="wrap flex justify-center align-top">
+
+            <div class="dao-card">
+                <div class="wrap flex justify-center align-top">
+                    <a href="https://dao.hypha.earth/digital-innovation-hub-LI/" target="_blank">
                         <div class="top-right-link">
                             <img src="<?php echo get_template_directory_uri(); ?>/img/icons/top-right-link.svg" />
                         </div>
-                        <img class="daologos"
-                            src="<?php echo get_template_directory_uri(); ?>/img/logos/logo-round.svg" />
-                        <div style="width:100%">
-                            <p class="text-18">
-                                LongNameForHere DAO
-                            </p>
-                        </div>
+
+                        <img class="daologos" src="<?php echo get_template_directory_uri(); ?>/img/daos/digihub.jpg" />
+                    </a>
+                    <div style="width:100%">
+                        <p class="text-18">
+                            DigiHub Liechtenstein
+                        </p>
                     </div>
                 </div>
-            </a>
+            </div>
+
+
+
+            <div class="dao-card">
+                <div class="wrap flex justify-center align-top">
+                    <a href="https://dao.hypha.earth/earthwise/" target="_blank">
+                        <div class="top-right-link">
+                            <img src="<?php echo get_template_directory_uri(); ?>/img/icons/top-right-link.svg" />
+                        </div>
+
+                        <img class="daologos"
+                            src="<?php echo get_template_directory_uri(); ?>/img/daos/earthwise.jpg" />
+                    </a>
+                    <div style="width:100%">
+                        <p class="text-18">
+                            EarthWise
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+
+
+            <div class="dao-card">
+                <div class="wrap flex justify-center align-top">
+                    <a href="https://dao.hypha.earth/teca/" target="_blank">
+                        <div class="top-right-link">
+                            <img src="<?php echo get_template_directory_uri(); ?>/img/icons/top-right-link.svg" />
+                        </div>
+
+                        <img class="daologos" src="<?php echo get_template_directory_uri(); ?>/img/daos/teca.png" />
+                    </a>
+                    <div style="width:100%">
+                        <p class="text-18">
+                            TECA
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+
+
+            <div class="dao-card">
+                <div class="wrap flex justify-center align-top">
+                    <a href="https://dao.hypha.earth/regenerate-x/" target="_blank">
+                        <div class="top-right-link">
+                            <img src="<?php echo get_template_directory_uri(); ?>/img/icons/top-right-link.svg" />
+                        </div>
+
+                        <img class="daologos" src="<?php echo get_template_directory_uri(); ?>/img/daos/regenx.jpg" />
+                    </a>
+                    <div style="width:100%">
+                        <p class="text-18">
+                            RegenerateX
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+
+
+            <div class="dao-card">
+                <div class="wrap flex justify-center align-top">
+                    <a href="https://dao.hypha.earth/regencivics/" target="_blank">
+                        <div class="top-right-link">
+                            <img src="<?php echo get_template_directory_uri(); ?>/img/icons/top-right-link.svg" />
+                        </div>
+
+                        <img class="daologos"
+                            src="<?php echo get_template_directory_uri(); ?>/img/daos/regencivics.png" />
+                    </a>
+                    <div style="width:100%">
+                        <p class="text-18">
+                            Regen Civics
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+
+
+            <div class="dao-card">
+                <div class="wrap flex justify-center align-top">
+                    <a href="https://dao.hypha.earth/dstudio/" target="_blank">
+                        <div class="top-right-link">
+                            <img src="<?php echo get_template_directory_uri(); ?>/img/icons/top-right-link.svg" />
+                        </div>
+
+                        <img class="daologos" src="<?php echo get_template_directory_uri(); ?>/img/daos/dstudio.jpg" />
+                    </a>
+                    <div style="width:100%">
+                        <p class="text-18">
+                            dStudio
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+
+
+            <div class="dao-card">
+                <div class="wrap flex justify-center align-top">
+                    <a href="https://dao.hypha.earth/hypha/explore/" target="_blank">
+                        <div class="top-right-link">
+                            <img src="<?php echo get_template_directory_uri(); ?>/img/icons/top-right-link.svg" />
+                        </div>
+
+                        <div class="icon big">
+                            <img src="<?php echo get_template_directory_uri(); ?>/img/icons/rocket.svg" />
+                        </div>
+                    </a>
+                    <div style="width:100%">
+                        <p class="text-16">
+                            And Many More!<br />
+                            <a class="light-blu" href="https://dao.hypha.earth/hypha/explore/" target="_blank">
+                                Discover all DAOs!
+                            </a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+
+
         </div>
     </div>
 </section>
