c28adf5b84305a9f1c91b63b65ab0669d8131937
Luigi Carlini <luigicarlini88@gmail.com>
2023-11-18 17:36:43 +0100

    mautic tacking in footer

--- footer.php
+++ footer.php
@@ -295,6 +295,22 @@ var FreeScoutW = {
 // Move it if you like, but keep it around.
 ?>
 
+
+<!-- mautic tracking pixel -->
+<script>
+(function(w, d, t, u, n, a, m) {
+    w['MauticTrackingObject'] = n;
+    w[n] = w[n] || function() {
+            (w[n].q = w[n].q || []).push(arguments)
+        }, a = d.createElement(t),
+        m = d.getElementsByTagName(t)[0];
+    a.async = 1;
+    a.src = u;
+    m.parentNode.insertBefore(a, m)
+})(window, document, 'script', 'https://mautic.hypha.earth/mtc.js', 'mt');
+
+mt('send', 'pageview');
+</script>
 </body>
 
 </html>
